832be82a3d66b87455922b891c6f76345b22a026
Nurzhan <nurzhankydyralieva@gmail.com>
2023-09-13 21:09:17 +0600

    second commit

--- src/test/java/com/example/jenkins_test/JenkinsTestApplicationTests.java
+++ src/test/java/com/example/jenkins_test/JenkinsTestApplicationTests.java
@@ -14,6 +14,7 @@ class JenkinsTestApplicationTests {
     @Test
     void contextLoads() {
         logger.info("Test case executing ...");
+        logger.info("Test case executing second log statement ...");
         assertEquals(true,true);
     }
 
