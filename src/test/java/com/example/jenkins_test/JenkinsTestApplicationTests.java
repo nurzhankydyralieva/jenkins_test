1bf2e8135b160e3342763c90004f7c8556666ca2
Nurzhan <nurzhankydyralieva@gmail.com>
2023-09-13 20:36:09 +0600

    first commit

---
+++ src/test/java/com/example/jenkins_test/JenkinsTestApplicationTests.java
@@ -0,0 +1,20 @@
+package com.example.jenkins_test;
+
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+class JenkinsTestApplicationTests {
+    public static Logger logger = LoggerFactory.getLogger(JenkinsTestApplicationTests.class);
+
+    @Test
+    void contextLoads() {
+        logger.info("Test case executing ...");
+        assertEquals(true,true);
+    }
+
+}
